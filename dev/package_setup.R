e42f673fd257d1bf31ade73c5843ff9630ab3e07
Danielle Dempsey <ddempsey@perennia.ca>
2022-08-23 13:39:16 -0300

    add file to correct folder

---
+++ dev/package_setup.R
@@ -0,0 +1,12 @@
+# August 23, 2022
+
+library(devtools)
+
+# set up project
+create_package("C:/Users/Danielle Dempsey/Desktop/RProjects/Packages/qaqcmar")
+
+# add license
+use_gpl3_license()
+
+# set up git repot
+use_git()
