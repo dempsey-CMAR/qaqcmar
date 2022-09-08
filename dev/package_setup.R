4b90684ff08f839da113d608e58a28d387cdbe0d
Danielle Dempsey <ddempsey@perennia.ca>
2022-09-08 16:12:45 -0300

    tweaks so that package passes R CMD check

--- dev/package_setup.R
+++ dev/package_setup.R
@@ -8,5 +8,23 @@ create_package("C:/Users/Danielle Dempsey/Desktop/RProjects/Packages/qaqcmar")
 # add license
 use_gpl3_license()
 
-# set up git repot
+# set up git repo
 use_git()
+
+# set up github
+use_github()
+
+# add packages
+use_package("dplyr")
+
+
+
+####
+
+dat_flag <- qc_flag_range(dat, threshold = 1)
+
+
+
+
+
+
