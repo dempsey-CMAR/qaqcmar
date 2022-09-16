1de3d1cb8e7e98e7effe13277e20f15592aff4dd
Danielle Dempsey <ddempsey@perennia.ca>
2022-09-16 16:47:24 -0300

    initial commit

---
+++ R/threshold_tables.R
@@ -0,0 +1,9 @@
+#' Example of compiled Hobo data in wide format, as exported from
+#' compile_HOBO_data()
+#'
+#' @format Default thresholds for adding flags to water quality data. A list
+#'   with a separate data frame for each test: climatology_table and
+#'   grossrange_table.
+#'
+#' @source Add references here
+"threshold_tables"
