ed2174f9062d0d087338e49907882f8390838d2d
Danielle Dempsey <ddempsey@perennia.ca>
2022-11-04 16:29:31 -0300

    update thresholds

--- data-raw/threshold_tables.R
+++ data-raw/threshold_tables.R
@@ -1,19 +1,19 @@
 #' @importFrom readxl read_excel
+#' @importFrom here here
 
-
-path <- system.file("data-raw", package = "qaqcmar")
+# path <- system.file("data-raw", package = "qaqcmar")
 
 
 climatology_table <- readxl::read_excel(
-  paste0(path, "/qc_thresholds.xlsx"), sheet = "climatology"
+ here::here("data-raw/qc_thresholds.xlsx"), sheet = "climatology"
 )
 
 seasons_table <- readxl::read_excel(
-  paste0(path, "/qc_thresholds.xlsx"), sheet = "seasons"
+  here::here("data-raw/qc_thresholds.xlsx"), sheet = "seasons"
 )
 
 grossrange_table <- readxl::read_excel(
-  paste0(path, "/qc_thresholds.xlsx"), sheet = "grossrange"
+  here::here("data-raw/qc_thresholds.xlsx"), sheet = "grossrange"
 )
 
 
@@ -26,50 +26,3 @@ threshold_tables <- list(
 )
 
 usethis::use_data(threshold_tables, overwrite = TRUE)
-
-
-
-# Old ---------------------------------------------------------------------
-
-# # might be better to read these in from csv files in data-raw
-# template <- data.frame(
-#   variable = c(
-#     "dissolved_oxygen_mg_per_L",
-#     "dissolved_oxygen_percent_saturation",
-#     "salinity_psu",
-#     "temperature_degree_C",
-#     "temperature_degree_C",
-#     "temperature_degree_C"
-#   ),
-#   sensor_make = c(
-#     "hobo",
-#     "aquameasure",
-#     "aquameasure",
-#     "aquameasure",
-#     "hobo",
-#     "vemco"
-#   )
-# )
-#
-# # climatology -------------------------------------------------------------
-#
-# seasons <- c("winter", "spring", "summer", "fall")
-#
-#
-# climatology_table <- expand.grid(
-#   variable = unique(template$variable), season = seasons
-# ) %>%
-#   cbind(
-#     season_min = c(2, 5, 10, 2, 70, 70, 70, 70, 0, 0, 0, 0, -0.7, 0, 7, 5),
-#     season_max = c(15, 20, 20, 15, 120, 130, 130, 120, 40, 40, 40, 40, 15, 20, 25, 20)
-#   )
-#
-# # gross range -------------------------------------------------------------
-# grossrange_table <- template %>%
-#   cbind(
-#     sensor_min = c(0, 0, 0, -5, -40, -5),
-#     sensor_max = c(30, 150, 40, 35, 70, 40),
-#     user_min = c(0, 50, 0, -3, -3, -3),
-#     user_max = c(25, 130, 35, 25, 25, 25)
-#   )
-
