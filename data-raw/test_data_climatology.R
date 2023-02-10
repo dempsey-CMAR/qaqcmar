54664cab730a863d53f677e9f5f89a5b7f03f3e4
Danielle Dempsey <ddempsey@perennia.ca>
2023-02-10 13:17:11 -0400

    simulated test data: faster and easier to generate

--- data-raw/test_data_climatology.R
+++ data-raw/test_data_climatology.R
@@ -4,224 +4,50 @@ library(lubridate)
 library(here)
 library(sensorstrings) # V. 0.1.0
 library(qaqcmar)       # V. 0.0.1
+library(tidyr)
 
 # Raw data ----------------------------------------------------------------
 
-depl1 <- ss_compile_deployment_data(here("data-raw/Wedgeport_2021-07-08")) %>%
-  mutate(dissolved_oxygen_uncorrected_mg_per_l = NA) %>%
-  relocate(
-    dissolved_oxygen_uncorrected_mg_per_l,
-    .after = dissolved_oxygen_percent_saturation
-  )
-
-# ss_ggplot_variables(depl1)
-
-# HOBO DO data from another deployment.
-depl2 <- ss_compile_deployment_data(
-   here("data-raw/Birchy Head_2021-11-26")
- ) %>%
-   mutate(
-     salinity_psu = NA,
-     temperature_degree_c = NA,
-     dissolved_oxygen_percent_saturation = NA,
-     sensor_depth_measured_m = NA
-   )
-
-do1 <- depl2 %>%
-  mutate(timestamp_utc = timestamp_utc - days(120))  # Align Birchy Head with Wedgeport
-
-do2 <- depl2 %>%
-  mutate(timestamp_utc = timestamp_utc + days(49))  # Align Birchy Head with Wedgeport
-
-
-# ss_ggplot_variables(depl2)
-
-dat_raw <- rbind(depl1, do1, do2) %>%
-  ss_convert_depth_to_ordered_factor()
-
-# ss_ggplot_variables(dat_raw)
-
-
-# extract 1 month from each season for test data -------------------------------------------
-
-# use only every sixth observation and remove extraneous columns to reduce size
-dat <- dat_raw %>%
-  mutate(numeric_month = month(timestamp_utc)) %>%
-  filter(
-    numeric_month %in% c(8, 11, 2, 5),
-    row_number() %% 30 == 0
-  ) %>%
-  select(-c(county, waterbody, station, lease, deployment_range, mooring_type)) %>%
-  ss_pivot_longer() %>%
-  filter(
-    !(variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        value < 13 & numeric_month == 11),
-    !(variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        value < 13 & numeric_month == 5),
-    !(variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        value > 15 & numeric_month == 11),
-    !(variable == "temperature_degree_c" &
-        value < -0.7 & numeric_month == 11)
-  ) %>%
-  ss_pivot_wider()
-
-# ss_ggplot_variables(dat)
-
-
-# timestamps for flagged observations ------------------------------------------------
-
-summer_low1 <- as_datetime("2021-08-07")
-summer_low2 <- summer_low1 + days(3)
-
-summer_high1 <- as_datetime("2021-08-15")
-summer_high2 <- summer_high1 + days(3)
-
-fall_low1 <- as_datetime("2021-11-07")
-fall_low2 <- fall_low1 + days(3)
-
-fall_high1 <- as_datetime("2021-11-15")
-fall_high2 <- fall_high1 + days(3)
-
-winter_low1 <- as_datetime("2022-02-07")
-winter_low2 <- winter_low1 + days(3)
-
-winter_high1 <- as_datetime("2022-02-15")
-winter_high2 <- winter_high1 + days(3)
-
-spring_low1 <- as_datetime("2022-05-07")
-spring_low2 <- spring_low1 + days(3)
-
-spring_high1 <- as_datetime("2022-05-15")
-spring_high2 <- spring_high1 + days(3)
-
-
-# values to flag ----------------------------------------------------------
-
-# base on thresholds so test will work even if thresholds are adjusted
-
 climatology_table <- threshold_tables %>%
-  filter(qc_test == "climatology") %>%
-  select(-c(qc_test, sensor_type)) %>%
-  mutate(
-    threshold = str_replace(threshold, "winter|spring|summer|fall", "season")
-    ) %>%
-  pivot_wider(names_from = "threshold", values_from = "threshold_value") %>%
-  mutate(
-    obs_low = case_when(
-      variable == "temperature_degree_c" | variable == "salinity_psu"  ~
-        season_min - 5,
-      TRUE ~  season_min  - 10
-    ),
-    obs_high = case_when(
-      variable == "temperature_degree_c" | variable == "salinity_psu" ~
-        season_max + 5,
-      TRUE ~  season_max + 10
+  filter(qc_test == "climatology", county == "Lunenburg") %>%
+  select(-c(qc_test, county, sensor_type)) %>%
+  pivot_wider(names_from = "threshold", values_from = "threshold_value")  %>%
+  bind_rows(
+    data.frame(
+      variable = rep("dissolved_oxygen_percent_saturation", 12),
+      month = seq(1, 12),
+      season_min = rep(95, 12),
+      season_max = rep(105, 12)
     )
   )
 
-#seasons_table <- threshold_tables$seasons_table
+months <- seq(1, 12)
+days <- rep(c(1, 10, 28), 12)
+variables = c("temperature_degree_c", "dissolved_oxygen_percent_saturation")
 
-# add observations to flag ------------------------------------------------
-
-dat_qc <- dat %>%
-  ss_pivot_longer() %>%
-  left_join(months_seasons, by = "numeric_month") %>%
-  left_join(climatology_table, by = c("season", "variable")) %>%
+dat <- expand.grid(month = months, day = days, variable = variables) %>%
+  distinct(month, day, variable) %>%
+  arrange(month) %>%
+  left_join(climatology_table, by = c("variable", "month")) %>%
   mutate(
-    # DO % saturation
+    sensor_depth_at_low_tide_m = 5,
+    timestamp_utc = as_datetime(paste("2023", month, day, sep = "-")),
     value = case_when(
-      variable == "dissolved_oxygen_percent_saturation" &
-      timestamp_utc > summer_low1 & timestamp_utc < summer_low2 ~ obs_low,
-      variable == "dissolved_oxygen_percent_saturation" &
-      timestamp_utc > summer_high1 & timestamp_utc < summer_high2 ~ obs_high,
-
-      variable == "dissolved_oxygen_percent_saturation" &
-      timestamp_utc > fall_low1 & timestamp_utc < fall_low2 ~ obs_low,
-      variable == "dissolved_oxygen_percent_saturation" &
-      timestamp_utc > fall_high1 & timestamp_utc < fall_high2 ~ obs_high,
-
-      variable == "dissolved_oxygen_percent_saturation" &
-      timestamp_utc > winter_low1 & timestamp_utc < winter_low2 ~ obs_low,
-      variable == "dissolved_oxygen_percent_saturation" &
-      timestamp_utc > winter_high1 & timestamp_utc < winter_high2 ~ obs_high,
-
-      variable == "dissolved_oxygen_percent_saturation" &
-      timestamp_utc > spring_low1 & timestamp_utc < spring_low2 ~ obs_low,
-      variable == "dissolved_oxygen_percent_saturation" &
-      timestamp_utc > spring_high1 & timestamp_utc < spring_high2 ~ obs_high,
-
-      # DO concentration
-      variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        timestamp_utc > summer_low1 & timestamp_utc < summer_low2 ~ obs_low,
-      variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        timestamp_utc > summer_high1 & timestamp_utc < summer_high2 ~ obs_high,
-
-      variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        timestamp_utc > fall_low1 & timestamp_utc < fall_low2 ~ obs_low,
-      variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        timestamp_utc > fall_high1 & timestamp_utc < fall_high2 ~ obs_high,
-
-      variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        timestamp_utc > winter_low1 & timestamp_utc < winter_low2 ~ obs_low,
-      variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        timestamp_utc > winter_high1 & timestamp_utc < winter_high2 ~ obs_high,
-
-      variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        timestamp_utc > spring_low1 & timestamp_utc < spring_low2 ~ obs_low,
-      variable == "dissolved_oxygen_uncorrected_mg_per_l" &
-        timestamp_utc > spring_high1 & timestamp_utc < spring_high2 ~ obs_high,
-
-      # temperature
-      variable == "temperature_degree_c" &
-        timestamp_utc > summer_low1 & timestamp_utc < summer_low2 ~ obs_low,
-      variable == "temperature_degree_c" &
-        timestamp_utc > summer_high1 & timestamp_utc < summer_high2 ~ obs_high,
-
-      variable == "temperature_degree_c" &
-        timestamp_utc > fall_low1 & timestamp_utc < fall_low2 ~ obs_low,
-      variable == "temperature_degree_c" &
-        timestamp_utc > fall_high1 & timestamp_utc < fall_high2 ~ obs_high,
-
-      variable == "temperature_degree_c" &
-        timestamp_utc > winter_low1 & timestamp_utc < winter_low2 ~ obs_low,
-      variable == "temperature_degree_c" &
-        timestamp_utc > winter_high1 & timestamp_utc < winter_high2 ~ obs_high,
-
-      variable == "temperature_degree_c" &
-        timestamp_utc > spring_low1 & timestamp_utc < spring_low2 ~ obs_low,
-      variable == "temperature_degree_c" &
-        timestamp_utc > spring_high1 & timestamp_utc < spring_high2 ~ obs_high,
-
-      # salinity
-      variable == "salinity_psu" &
-        timestamp_utc > summer_low1 & timestamp_utc < summer_low2 ~ obs_low,
-      variable == "salinity_psu" &
-        timestamp_utc > summer_high1 & timestamp_utc < summer_high2 ~ obs_high,
-
-      variable == "salinity_psu" &
-        timestamp_utc > fall_low1 & timestamp_utc < fall_low2 ~ obs_low,
-      variable == "salinity_psu" &
-        timestamp_utc > fall_high1 & timestamp_utc < fall_high2 ~ obs_high,
-
-      variable == "salinity_psu" &
-        timestamp_utc > winter_low1 & timestamp_utc < winter_low2 ~ obs_low,
-      variable == "salinity_psu" &
-        timestamp_utc > winter_high1 & timestamp_utc < winter_high2 ~ obs_high,
-
-      variable == "salinity_psu" &
-        timestamp_utc > spring_low1 & timestamp_utc < spring_low2 ~ obs_low,
-      variable == "salinity_psu" &
-        timestamp_utc > spring_high1 & timestamp_utc < spring_high2 ~ obs_high,
-
-      TRUE ~ value
+      day == 1 ~ season_min - 5,
+      day == 10 ~ (season_min + season_max) / 2,
+      day == 28 ~ season_max + 5,
+      TRUE ~ NA_real_
     )
   ) %>%
-  select(-c(season, season_min, season_max, obs_low, obs_high, numeric_month)) %>%
-  ss_pivot_wider()
+  select(-c(month, day, season_min, season_max)) %>%
+  ss_pivot_wider() %>%
+  arrange(timestamp_utc)
 
-ss_ggplot_variables(dat_qc)
 
+
+ss_ggplot_variables(dat) +
+  geom_point(size = 3)
 
 # Export rds file
-saveRDS(dat_qc, file = here("inst/testdata/test_data_climatology.RDS"))
+saveRDS(dat, file = here("inst/testdata/test_data_climatology.RDS"))
 
--- data-raw/test_data_grossrange.R
+++ data-raw/test_data_grossrange.R
@@ -1,9 +1,21 @@
-# library(readr)
+# February 10, 2023
+
+# Simulate data for the gross range test
+# Data constructed so there are 4 observations per month
+# Day 1: Gross Range Flag 4 (low)
+# Day 5: Gross Range Flag 3 (low)*
+# Day 10: Gross Range Flag 1
+# Day 15: Gross Range Flag 4 (high)
+# Day 28: Gross Range Flag 3 (high)
+
+# * for the aquameasure and vr2ar sensors, the user_min is less than the sensor_min,
+# which means a flag of 3 (low) will not be assigned
+
 # library(dplyr)
 # library(lubridate)
 # library(here)
-# library(sensorstrings) # V. 0.1.0
-# library(qaqcmar)       # V. 0.0.0.9000
+# library(sensorstrings)
+# library(qaqcmar)
 
 
 #' @importfrom dplyr %>% filter mutate relocate select
@@ -11,222 +23,125 @@
 #' @importFrom lubridate as_datetime days
 #' @importFrom sensorstrings  ss_convert_depth_to_ordered_factor ss_ggplot_variables
 
-
-
 # Raw data ----------------------------------------------------------------
 
-dat <- ss_compile_deployment_data(here("data-raw/Wedgeport_2021-07-08")) %>%
-  mutate(dissolved_oxygen_uncorrected_mg_per_l = NA) %>%
-  relocate(
-    dissolved_oxygen_uncorrected_mg_per_l,
-    .after = dissolved_oxygen_percent_saturation
-  )
-
- #ss_ggplot_variables(dat)
-
-# HOBO DO data from another deployment.
- hobo_do <- ss_compile_deployment_data(
-   here("data-raw/Birchy Head_2021-11-26")
- ) %>%
-  mutate(
-    salinity_psu = NA,
-    dissolved_oxygen_percent_saturation = NA,
-    sensor_depth_measured_m = NA) %>%
-  # Align Birchy Head January with Wedgeport October
-  mutate(timestamp_utc = timestamp_utc - days(60))
-
-# ss_ggplot_variables(hobo_do)
-
-dat <- rbind(dat, hobo_do) %>%
-  ss_convert_depth_to_ordered_factor()
-
-#ss_ggplot_variables(dat)
-
-
-# extract 1 month for test data -------------------------------------------
-
-# use only every sixth observation and remove extraneous columns to reduce size
-oct_dat <-  dat %>%
-  filter(
-  (timestamp_utc > as_datetime("2021-10-01 00:00:00") &
-    timestamp_utc < as_datetime("2021-11-01 00:00:00")),
-  row_number() %% 6 == 0
-  ) %>%
-  select(-county, -waterbody, -station, -lease, -deployment_range, -mooring_type)
-
-#ss_ggplot_variables(oct_dat)
-
-
-# add observations to flag ------------------------------------------------
-
-# For each temperature and the aM DO series, the sets of anomalous points will be in this order:
-# 1. Gross Range Flag 4 (low)
-# 2. Gross Range Flag 4 (high)
-# 3. Gross Range Flag 3 (low)
-# 4. Gross Range Flag 3 (high)
-# 5. Climatology Flag 3 (fall, low)
-# 6. Climatology Flag 3 (fall, high)
-
-# For the Hobo DO series, the order is:
-# 1. Gross Range Flag 4 (low)
-# 2. Gross Range Flag 4 (high)
-# 3. Gross Range Flag 3 (high)
-# 5. Climatology Flag 3 (fall/winter, low)
-# 6. Climatology Flag 3 (fall/winter, high)
-
-# For the salinity series, the order is:
-# 1. Gross Range Flag 4 (low)
-# 2. Gross Range Flag 4 (high)
-# 3. Gross Range Flag 3 (high)
-
-qaqc_dat <- oct_dat %>%
-  mutate(
-    # aM DO
-    dissolved_oxygen_percent_saturation = case_when(
-      timestamp_utc > as_datetime("2021-10-03 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-03 06:00:00") ~
-        dissolved_oxygen_percent_saturation * -1,
-      timestamp_utc > as_datetime("2021-10-07 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-07 06:00:00") ~
-        dissolved_oxygen_percent_saturation + 55,
-      timestamp_utc > as_datetime("2021-10-13 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-13 06:00:00") ~
-        dissolved_oxygen_percent_saturation - 70,
-      timestamp_utc > as_datetime("2021-10-17 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-17 06:00:00") ~
-        dissolved_oxygen_percent_saturation + 35,
-      timestamp_utc > as_datetime("2021-10-22 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-22 06:00:00") ~
-        dissolved_oxygen_percent_saturation - 35,
-      timestamp_utc > as_datetime("2021-10-28 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-28 06:00:00") ~
-        dissolved_oxygen_percent_saturation + 25,
-      TRUE ~ dissolved_oxygen_percent_saturation
-    ),
-    # Hobo DO
-    dissolved_oxygen_uncorrected_mg_per_l = case_when(
-      timestamp_utc > as_datetime("2021-10-03 12:00:00") &
-        timestamp_utc < as_datetime("2021-10-03 18:00:00") ~
-        dissolved_oxygen_uncorrected_mg_per_l * -1,
-      timestamp_utc > as_datetime("2021-10-10 12:00:00") &
-        timestamp_utc < as_datetime("2021-10-10 18:00:00") ~
-        dissolved_oxygen_uncorrected_mg_per_l + 30,
-      timestamp_utc > as_datetime("2021-10-17 12:00:00") &
-        timestamp_utc < as_datetime("2021-10-17 18:00:00") ~
-        dissolved_oxygen_uncorrected_mg_per_l + 15,
-      timestamp_utc > as_datetime("2021-10-24 12:00:00") &
-        timestamp_utc < as_datetime("2021-10-24 18:00:00") ~
-        dissolved_oxygen_uncorrected_mg_per_l - 11,
-      timestamp_utc > as_datetime("2021-10-30 12:00:00") &
-        timestamp_utc < as_datetime("2021-10-30 18:00:00") ~
-        dissolved_oxygen_uncorrected_mg_per_l + 5,
-      TRUE ~ dissolved_oxygen_uncorrected_mg_per_l
-    ),
-    # aM Sal
-    salinity_psu = case_when(
-      timestamp_utc > as_datetime("2021-10-06 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-06 06:00:00") ~
-        salinity_psu * -1,
-      timestamp_utc > as_datetime("2021-10-16 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-16 06:00:00") ~
-        salinity_psu + 20,
-      timestamp_utc > as_datetime("2021-10-26 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-26 06:00:00") ~
-        salinity_psu + 10,
-      TRUE ~ salinity_psu
-    ),
-    # aM Temp
-    temperature_degree_c = case_when(
-      sensor_serial_number == 670354 &
-        timestamp_utc > as_datetime("2021-10-02 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-02 06:00:00") ~
-        temperature_degree_c * -1,
-      sensor_serial_number == 670354 &
-        timestamp_utc > as_datetime("2021-10-08 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-08 06:00:00") ~
-        temperature_degree_c + 25,
-      sensor_serial_number == 670354 &
-        timestamp_utc > as_datetime("2021-10-12 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-12 06:00:00") ~
-        temperature_degree_c - 19,
-      sensor_serial_number == 670354 &
-        timestamp_utc > as_datetime("2021-10-18 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-18 06:00:00") ~
-        temperature_degree_c + 15,
-      sensor_serial_number == 670354 &
-        timestamp_utc > as_datetime("2021-10-21 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-21 06:00:00") ~
-        temperature_degree_c - 15,
-      sensor_serial_number == 670354 &
-        timestamp_utc > as_datetime("2021-10-27 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-27 06:00:00") ~
-        temperature_degree_c + 7,
-      # Hobo Temp
-      sensor_serial_number == 20495248 &
-        timestamp_utc > as_datetime("2021-10-04 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-04 06:00:00") ~
-        temperature_degree_c - 60,
-      sensor_serial_number == 20495248 &
-        timestamp_utc > as_datetime("2021-10-09 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-09 06:00:00") ~
-        temperature_degree_c + 60,
-      sensor_serial_number == 20495248 &
-        timestamp_utc > as_datetime("2021-10-14 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-14 06:00:00") ~
-        temperature_degree_c - 19,
-      sensor_serial_number == 20495248 &
-        timestamp_utc > as_datetime("2021-10-19 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-19 06:00:00") ~
-        temperature_degree_c + 10,
-      sensor_serial_number == 20495248 &
-        timestamp_utc > as_datetime("2021-10-23 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-23 06:00:00") ~
-        temperature_degree_c - 15,
-      sensor_serial_number == 20495248 &
-        timestamp_utc > as_datetime("2021-10-29 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-29 06:00:00") ~
-        temperature_degree_c + 10,
-      # Vemco Temp
-      sensor_serial_number == 549340 &
-        timestamp_utc > as_datetime("2021-10-05 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-05 12:00:00") ~
-        temperature_degree_c * -1,
-      sensor_serial_number == 549340 &
-        timestamp_utc > as_datetime("2021-10-10 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-10 12:00:00") ~
-        temperature_degree_c + 25,
-      sensor_serial_number == 549340 &
-        timestamp_utc > as_datetime("2021-10-15 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-15 12:00:00") ~
-        temperature_degree_c - 20,
-      sensor_serial_number == 549340 &
-        timestamp_utc > as_datetime("2021-10-20 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-20 12:00:00") ~
-        temperature_degree_c + 15,
-      sensor_serial_number == 549340 &
-        timestamp_utc > as_datetime("2021-10-25 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-25 12:00:00") ~
-        temperature_degree_c - 15,
-      sensor_serial_number == 549340 &
-        timestamp_utc > as_datetime("2021-10-30 00:00:00") &
-        timestamp_utc < as_datetime("2021-10-30 12:00:00") ~
-        temperature_degree_c + 10,
-      TRUE ~ temperature_degree_c
+# import the gross range table
+grossrange_table <- threshold_tables %>%
+  filter(qc_test == "grossrange", county == "Lunenburg" | is.na(county)) %>%
+  select(-c(qc_test, county, month)) %>%
+  pivot_wider(names_from = "threshold", values_from = "threshold_value") %>%
+  # placeholder
+  bind_rows(
+    data.frame(
+      variable = rep("dissolved_oxygen_percent_saturation"),
+      user_min = 80, user_max = 120
     )
-
+  ) %>%
+  filter(
+    variable == "temperature_degree_c" |
+      variable == "dissolved_oxygen_percent_saturation"
   )
 
-#ss_ggplot_variables(qaqc_dat)
+# vectors used to create data frame
+months <- seq(1, 12)
+days <- rep(c(1, 5, 10, 15, 28), 12)
+variables = c("temperature_degree_c", "dissolved_oxygen_percent_saturation")
+#sensors = na.omit(unique(grossrange_table$sensor_type))
+sensors <- c("aquameasure", "hobo", "tidbit", "vr2ar")
 
+dat <- expand.grid(
+  month = months, day = days, variable = variables, sensor_type = sensors
+) %>%
+  distinct(month, day, variable, sensor_type) %>%
+  # filter out impossible sensor-variable combinations
+  filter(
+    !((variable == "dissolved_oxygen_percent_saturation" |
+         variable == "dissolved_oxygen_mg_per_l") &
+        (sensor_type == "tidbit" | sensor_type == "hobo" | sensor_type == "vr2ar")),
+    !(variable == "dissolved_oxygen_percent_saturation" & sensor_type == "hobo do")
+  ) %>%
+  # join with the sensor and user thresholds
+  left_join(
+    grossrange_table %>%
+      select(sensor_type, variable, contains("sensor")) %>%
+      filter(!is.na(sensor_type)),
+    by = c("sensor_type", "variable")
+  ) %>%
+  left_join(
+    grossrange_table %>%
+      select(variable, contains("user")) %>%
+      filter(!is.na(user_min) & !is.na(user_max)),
+    by = "variable"
+  )  %>%
+  mutate(
+    # add depths (so easier to view simulated data when plotted)
+    sensor_depth_at_low_tide_m = case_when(
+      sensor_type == "hobo" ~ 2,
+      sensor_type == "aquameasure" ~ 5,
+      sensor_type == "hobo do" ~ 10,
+      sensor_type == "tidbit" ~ 15,
+      sensor_type == "vr2ar" ~ 25,
+       TRUE ~ NA_real_
+    ),
+    timestamp_utc = as_datetime(paste("2023", month, day, sep = "-")),
+    # add values with known flags
+    value = case_when(
+      day == 1 ~ sensor_min - 5, # flag 4
+      day == 5 ~ sensor_min + 1, # flag 3
+      day == 10 ~ case_when(     # flag 1
+        variable == "temperature_degree_c" & sensor_type == "hobo" ~ 2,
+        variable == "temperature_degree_c" & sensor_type == "aquameasure" ~ 5,
+        variable == "temperature_degree_c" & sensor_type == "hobo do" ~ 10,
+        variable == "temperature_degree_c" & sensor_type == "tidbit" ~ 15,
+        variable == "temperature_degree_c" & sensor_type == "vr2ar" ~ 19,
+
+        variable == "dissolved_oxygen_percent_saturation" ~ 100,
+        TRUE ~ NA_real_
+      ),
+      day == 15 ~ sensor_max + 5, # flag 4
+      day == 28 ~ sensor_max - 1, # flag 3
+      TRUE ~ NA_real_
+    )
+  ) %>%
+  select(-c(month, sensor_min, sensor_max, user_min, user_max)) %>%
+  ss_pivot_wider()
+
+
+dat[which((dat$sensor_type == "aquameasure" |
+            dat$sensor_type == "vr2ar") & dat$day == 5),
+    "temperature_degree_c"
+  ] <- NA
+
+dat[which(dat$sensor_type == "v2rar" & dat$day == 5),
+    "temperature_degree_c"
+] <- NA
+
+dat <- dat %>% select(-day)
 
 # Export rds file
-saveRDS(qaqc_dat, file = here("inst/testdata/test_data_grossrange.RDS"))
+saveRDS(dat, file = here("inst/testdata/test_data_grossrange.RDS"))
 
-# # some quick checks
-# gr_dat <- qc_test_grossrange(qaqc_dat) #%>%
-# qc_pivot_longer(qc_tests = "grossrange")
-# qc_plot_flag_vars(gr_dat, qc_tests = "grossrange")
+
+# ss_ggplot_variables(dat) +
+#   geom_point(size = 3)
 #
-# cl_dat <- qc_test_climatology(qaqc_dat) %>%
-#   qc_pivot_longer(qc_tests = "climatology")
-# qc_plot_flag_vars(cl_dat, qc_tests = "climatology")
+
+
+# qc_gr <- dat %>%
+#   qc_test_grossrange(county = "Lunenburg") %>%
+#   mutate(sensor_serial_number = "")
+#
+#
+# qc_plot_flags(
+#   qc_gr,
+#   qc_tests = "grossrange",
+#   vars = "temperature_degree_c"
+# )
+#
+# qc_plot_flags(
+#   qc_gr,
+#   qc_tests = "grossrange",
+#   vars = "dissolved_oxygen_percent_saturation"
+# )
+
+
