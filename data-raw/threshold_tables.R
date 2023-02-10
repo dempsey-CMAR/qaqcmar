ad1e423c376ee072615b293b9ad6f969734551fc
Danielle Dempsey <ddempsey@perennia.ca>
2023-02-10 13:17:51 -0400

    new thresholds as generated from thresholds analysis

--- data-raw/threshold_tables.R
+++ data-raw/threshold_tables.R
@@ -5,45 +5,48 @@
 
 # path <- system.file("data-raw", package = "qaqcmar")
 
-months_seasons <- readxl::read_excel(
-  here::here("data-raw/qc_thresholds.xlsx"), sheet = "seasons"
+threshold_tables <- read_csv(
+  here("data-raw/threshold_tables.csv"), show_col_types = FALSE
 )
 
-climatology_table <- readxl::read_excel(
- here::here("data-raw/qc_thresholds.xlsx"), sheet = "climatology", na = "NA"
-)
+usethis::use_data(threshold_tables, overwrite = TRUE)
 
-grossrange_table <- readxl::read_excel(
-  here::here("data-raw/qc_thresholds.xlsx"), sheet = "grossrange"
-)
 
-spike_table <- readxl::read_excel(
-  here::here("data-raw/qc_thresholds.xlsx"), sheet = "spike"
-)
+# climatology_table <- readxl::read_excel(
+#  here::here("data-raw/qc_thresholds.xlsx"), sheet = "climatology", na = "NA"
+# )
+#
+# grossrange_table <- readxl::read_excel(
+#   here::here("data-raw/qc_thresholds.xlsx"), sheet = "grossrange"
+# )
+#
+# spike_table <- readxl::read_excel(
+#   here::here("data-raw/qc_thresholds.xlsx"), sheet = "spike"
+# )
 
 # merge and format --------------------------------------------------------
 
-threshold_tables <- climatology_table %>%
-  pivot_longer(cols = c("season_min", "season_max"), names_to = "threshold") %>%
-  mutate(threshold = str_remove(threshold, pattern = "season_"),
-         threshold = paste(season, threshold, sep = "_")) %>%
-  mutate(qc_test = "climatology") %>%
- # select(-season) %>%
-  bind_rows(
-    grossrange_table %>%
-      pivot_longer(cols = sensor_min:user_max, names_to = "threshold") %>%
-      mutate(
-        sensor_type = if_else(sensor_type == "vemco", "vr2ar", sensor_type),
-        qc_test = "grossrange", threshold = paste0(sensor_type, "_", threshold),
-        threshold = str_replace(threshold, "aquameasure", "am")
-      )
-  ) %>% bind_rows(
-   spike_table %>%
-      pivot_longer(cols = c("spike_high", "spike_low"), names_to = "threshold") %>%
-      mutate(qc_test = "spike")
-  ) %>%
-  mutate(sensor_type = if_else(sensor_type == "vemco", "vr2ar", sensor_type)) %>%
-  select(qc_test, variable, sensor_type, season, threshold, threshold_value = value)
+# threshold_tables <- climatology_table %>%
+#   pivot_longer(cols = c("season_min", "season_max"), names_to = "threshold") %>%
+#   mutate(threshold = str_remove(threshold, pattern = "season_"),
+#          threshold = paste(season, threshold, sep = "_")) %>%
+#   mutate(qc_test = "climatology") %>%
+#  # select(-season) %>%
+#   bind_rows(
+#     grossrange_table %>%
+#       pivot_longer(cols = sensor_min:user_max, names_to = "threshold") %>%
+#       mutate(
+#         sensor_type = if_else(sensor_type == "vemco", "vr2ar", sensor_type),
+#         qc_test = "grossrange", threshold = paste0(sensor_type, "_", threshold),
+#         threshold = str_replace(threshold, "aquameasure", "am")
+#       )
+#   ) %>% bind_rows(
+#    spike_table %>%
+#       pivot_longer(cols = c("spike_high", "spike_low"), names_to = "threshold") %>%
+#       mutate(qc_test = "spike")
+#   ) %>%
+#   mutate(sensor_type = if_else(sensor_type == "vemco", "vr2ar", sensor_type)) %>%
+#   select(qc_test, variable, sensor_type, season, threshold, threshold_value = value)
 
 
 # export ------------------------------------------------------------------
