8f5baed324fa1cd2bd23c883b3d5a65dd2977f16
Danielle Dempsey <ddempsey@perennia.ca>
2023-06-20 15:09:10 -0300

    uses only lagged values

--- R/qc_test_flat_line.R
+++ R/qc_test_flat_line.R
@@ -1,5 +1,7 @@
 #' Add flag columns for the flat line test
 #'
+#' Only checks if values are *equal* (no tolerance)
+#'
 #' @param dat Data frame of sensor string data in wide format.
 #'
 #' @return placeholder for now
@@ -15,50 +17,81 @@
 #'
 #' @export
 
-qc_test_spike <- function(dat) {
+qc_test_spike <- function(dat, flat_line_table = NULL, keep_lag_cols = FALSE) {
 
-  # # import default thresholds from internal data file
-  # if (is.null(spike_table)) {
-  #   spike_table <- threshold_tables %>%
-  #     filter(qc_test == "spike") %>%
-  #     select(-c(qc_test, month, sensor_type)) #%>%
-  #    # pivot_wider(names_from = "threshold", values_from = "threshold_value")
-  # }
+  # import default thresholds from internal data file
+  if (is.null(flat__table)) {
+    flat_line_table <- threshold_tables %>%
+      filter(qc_test == "flat_line") %>%
+      select(-c(qc_test, month, sensor_type))
+  }
 #
 #   spike_table <- spike_table %>%
 #     select(variable, threshold, threshold_value)
 
-  dat %>%
+  flat_line_table <- data.frame(
+    count_suspect = 3,
+    count_fail = 5
+  )
+
+  cols_suspect <- c(
+    "value",
+    paste("value_lag", 1:(flat_line_table$count_suspect-1), sep = "")
+  )
+
+  cols_fail <- c(
+    "value",
+    paste("value_lag", 1:(flat_line_table$count_fail-1), sep = "")
+  )
+
+
+  x <- dat %>%
     ss_pivot_longer() %>%
-    left_join(spike_table, by = "variable") %>%
-    pivot_wider(names_from = "threshold", values_from = "threshold_value") %>%
     mutate(sensor = paste(sensor_type, sensor_serial_number, sep = "-")) %>%
     group_by(sensor, variable) %>%
     dplyr::arrange(timestamp_utc, .by_group = TRUE) %>%
+    add_n_lag_columns(value, 1:flat_line_table$count_fail) %>%
+    rowwise() %>%
     mutate(
-      lag_value = lag(value),
-      lead_value = lead(value),
-      spike_ref = (lag_value + lead_value) / 2,
-      spike_value = abs(value - spike_ref),
-      spike_flag = case_when(
-        spike_value > spike_high ~ 4,
-        (spike_value <= spike_high & spike_value > spike_low) ~ 3,
-        spike_value <= spike_low ~ 1,
-        TRUE ~ 2
+      # TRUE if value, value_lag1... value_lag_suspect are the same value
+      suspect = n_distinct(c_across(all_of(cols_suspect))) == 1,
+      # TRUE if value, value_lag1... value_lag_fail are the same value
+      fail = n_distinct(c_across(all_of(cols_fail))) == 1,
+
+      flat_line_flag = case_when(
+        isTRUE(fail) ~ 4,
+        isTRUE(suspect) ~ 3,
+
+        #  is.na(suspect) & is.na(fail) ~ 2,
+
+        TRUE ~ 1
       ),
-      spike_flag = ordered(spike_flag, levels = 1:4)
+      flat_line_flag = ordered(flat_line_flag, levels = 1:4)
     ) %>%
     ungroup() %>%
-    #remove extra columns
-    select(
-      -c(lag_value, lead_value, spike_ref, spike_value, sensor,
-         spike_high, spike_low)
-    ) %>%
     pivot_wider(
       names_from = variable,
-      values_from = c(value, spike_flag),
+      values_from = c(value, flat_line_flag),
       names_sort = TRUE
     )
+
+
+
+   qc_plot_flags(x, qc_tests = "flat_line")
+#
+# # NAs for when lag vals are NA
+#   # suspect for when 4 in a row
+#     ungroup() %>%
+#     #remove extra columns
+#     select(
+#       -c(lag_value, lead_value, spike_ref, spike_value, sensor,
+#          spike_high, spike_low)
+#     ) %>%
+#     pivot_wider(
+#       names_from = variable,
+#       values_from = c(value, spike_flag),
+#       names_sort = TRUE
+#     )
 
 }
 
