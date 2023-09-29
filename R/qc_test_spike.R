97160b027e0cea5530a6de1a32b96810b61db1e6
Danielle Dempsey <ddempsey@perennia.ca>
2023-09-29 08:23:23 -0300

    no errors on local R CMD Check

--- R/qc_test_spike.R
+++ R/qc_test_spike.R
@@ -26,7 +26,7 @@
 qc_test_spike <- function(dat, spike_table = NULL) {
   # import default thresholds from internal data file
   if (is.null(spike_table)) {
-    spike_table <- threshold_tables %>%
+    spike_table <- thresholds %>%
       filter(qc_test == "spike") %>%
       select(-c(qc_test, month, sensor_type)) # %>%
     # pivot_wider(names_from = "threshold", values_from = "threshold_value")
