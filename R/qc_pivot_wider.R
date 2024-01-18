dff43ec70580ff75d91065d3ab374a6eb55fbdb8
Danielle Dempsey <ddempsey@perennia.ca>
2024-01-18 12:28:46 -0400

    use values_from argument more effectively to majorly simplify function

--- R/qc_pivot_wider.R
+++ R/qc_pivot_wider.R
@@ -2,83 +2,88 @@
 #'
 #' @param dat_long Data frame of flagged sensor string data in long format.
 #'
-#' @param vars Variables included in \code{dat_long}.
-#'
-#' @param var_position Character string. Name of the column in \code{dat_long}
-#'   that the the variable column will be located after. By default, each
-#'   variable column will be located after the column
-#'   \code{sensor_depth_at_low_tide_m}.
-#'
 #' @return Returns \code{dat_long}, with variables and flags pivoted to a wide
 #'   format (a separate column for each variable and variable-qc_test
 #'   combination).
 #'
-#' @importFrom dplyr %>% all_of any_of arrange filter left_join rename
-#' @importFrom purrr reduce
-#' @importFrom tidyr pivot_longer
+#' @importFrom dplyr %>% contains
+#' @importFrom tidyr pivot_wider
+#' @importFrom stringr str_remove_all
 #'
 #' @export
 
-qc_pivot_wider <- function(dat_long, vars = NULL, var_position = NULL) {
+qc_pivot_wider <- function(dat_long) {
 
-  if (is.null(vars)) {
-    vars <- sort(unique(dat_long$variable))
-  }
+  #check if depth_crosscheck will ever get caught in this
 
-  first_cols <- c(
-    "county", "waterbody", "station", "lease", "latitude", "longitude",
-    "deployment_range", "string_configuration",
-    "sensor_type", "sensor_serial_number",
-    "timestamp_utc", "sensor_depth_at_low_tide_m", "depth_crosscheck_flag"
-  )
-
-  dat_out <- list(NULL)
-
-  if("dissolved_oxygen_percent_saturation" %in% vars) {
-    dat_out[[1]] <- pivot_flags_wider(
-      dat_long,
-      var = "dissolved_oxygen_percent_saturation"
-    )
-  }
-
-  if("dissolved_oxygen_uncorrected_mg_per_l" %in% vars) {
-    dat_out[[2]] <- pivot_flags_wider(
-      dat_long,
-      var = "dissolved_oxygen_uncorrected_mg_per_l"
-    )
-  }
-
-  if("salinity_psu" %in% vars) {
-    dat_out[[3]] <- pivot_flags_wider(dat_long, var = "salinity_psu")
-  }
-
-  if("sensor_depth_measured_m" %in% vars) {
-    dat_out[[4]] <- pivot_flags_wider(dat_long, var = "sensor_depth_measured_m")
-  }
-
-  if("temperature_degree_c" %in% vars) {
-    dat_out[[5]] <- pivot_flags_wider(dat_long, var = "temperature_degree_c")
-  }
-
-  # remove empty list elements
-  dat_out2 <- Filter(Negate(is.null), dat_out)
-
-  join_cols <- first_cols[first_cols %in% colnames(dat_long)]
-
-  # join by all common columns
-  dat_out3 <- dat_out2 %>%
-    purrr::reduce(dplyr::left_join, by = join_cols) %>%
-    select(
-      any_of(first_cols),
-      all_of(vars),
-      contains("depth_crosscheck"),
-      contains("climatology"),
-      contains("grossrange"),
-      contains("rolling_sd"),
-      contains("spike")
+  dat <- dat_long %>%
+    pivot_wider(
+      names_from = variable,
+      values_from = c(value, contains("flag_value"))
     )
 
-  dat_out3
+  colnames(dat) <- str_remove_all(colnames(dat), pattern = "value_")
+
+  dat
+
+  # if (is.null(vars)) {
+  #   vars <- sort(unique(dat_long$variable))
+  # }
+  #
+  # first_cols <- c(
+  #   "county", "waterbody", "station", "lease", "latitude", "longitude",
+  #   "deployment_range", "string_configuration",
+  #   "sensor_type", "sensor_serial_number",
+  #   "timestamp_utc", "sensor_depth_at_low_tide_m", "depth_crosscheck_flag"
+  # )
+  #
+  # dat_out <- list(NULL)
+  #
+  # if("dissolved_oxygen_percent_saturation" %in% vars) {
+  #   dat_out[[1]] <- pivot_flags_wider(
+  #     dat_long,
+  #     var = "dissolved_oxygen_percent_saturation"
+  #   )
+  # }
+  #
+  # if("dissolved_oxygen_uncorrected_mg_per_l" %in% vars) {
+  #   dat_out[[2]] <- pivot_flags_wider(
+  #     dat_long,
+  #     var = "dissolved_oxygen_uncorrected_mg_per_l"
+  #   )
+  # }
+  #
+  # if("salinity_psu" %in% vars) {
+  #   dat_out[[3]] <- pivot_flags_wider(dat_long, var = "salinity_psu")
+  # }
+  #
+  # if("sensor_depth_measured_m" %in% vars) {
+  #   dat_out[[4]] <- pivot_flags_wider(dat_long, var = "sensor_depth_measured_m")
+  # }
+  #
+  # if("temperature_degree_c" %in% vars) {
+  #   dat_out[[5]] <- pivot_flags_wider(dat_long, var = "temperature_degree_c")
+  # }
+  #
+  # # remove empty list elements
+  # dat_out2 <- Filter(Negate(is.null), dat_out)
+  #
+  # join_cols <- first_cols[first_cols %in% colnames(dat_long)]
+  #
+  # # join by all common columns
+  # dat_out3 <- dat_out2 %>%
+  #   purrr::reduce(dplyr::left_join, by = join_cols) %>%
+  #   select(
+  #     any_of(first_cols),
+  #     all_of(vars),
+  #     contains("depth_crosscheck"),
+  #     contains("climatology"),
+  #     contains("grossrange"),
+  #     contains("rolling_sd"),
+  #     contains("spike")
+  #   )
+  #
+  # dat_out3
 }
 
 
