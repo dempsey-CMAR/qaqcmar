87f73ca2dd796f062b716f1ff428b07a0f0fc05f
Danielle Dempsey <ddempsey@perennia.ca>
2024-01-18 19:25:28 -0400

    add option to export max flags or all flags

--- R/qc_assign_max_flag.R
+++ R/qc_assign_max_flag.R
@@ -9,34 +9,81 @@
 #'
 #' @param qc_tests Quality control tests included in \code{dat}. Default is
 #'   \code{qc_tests = c("climatology", "grossrange", "rolling_sd", "spike")}.
+
+#' @param return_all Logical value indicating whether to return all quality
+#'   control flag columns or only the summary columns. If \code{TRUE}, all flag
+#'   columns will be returned. If \code{FALSE}, only the summary columns will be
+#'   returned. Default is \code{TRUE}.
 #'
-#' @return Returns \code{dat} in a wide format, with a single flag
-#'   column for each variable column.
+#' @return Returns \code{dat} in a wide format, with a single flag column for
+#'   each variable column.
 #'
-#' @importFrom dplyr %>% any_of contains everything mutate rename select
+#' @importFrom dplyr %>% all_of any_of contains everything left_join mutate
+#'   rename select
 #' @importFrom purrr pmap
 #' @importFrom stringr str_remove_all
 #' @importFrom tidyr pivot_wider
 #'
 #' @export
 
-qc_assign_max_flag <- function(dat, qc_tests = NULL) {
+qc_assign_max_flag <- function(dat, qc_tests = NULL, return_all = TRUE) {
 
   if(is.null(qc_tests)) {
     qc_tests = c("climatology", "grossrange", "rolling_sd", "spike")
   }
 
+  # use for the join and to order columns in output
+  depl_cols <- c(
+    "county",
+    "waterbody",
+    "station",
+    "lease",
+    "latitude" ,
+    "longitude" ,
+    "deployment_range"   ,
+    "string_configuration",
+    "sensor_type"     ,
+    "sensor_serial_number"  ,
+    "timestamp_utc"  ,
+    "sensor_depth_at_low_tide_m",
+    "depth_crosscheck_flag"
+  )
+
+  qc_test_cols <- thresholds %>%
+    select(qc_test, variable) %>%
+    distinct() %>%
+    mutate(col_name = paste(qc_test, "flag", variable, sep = "_")) %>%
+    arrange(qc_test)
+  qc_test_cols <- qc_test_cols$col_name
+
+  qc_max_cols <- c(
+    "qc_flag_dissolved_oxygen_percent_saturation"   ,
+    "qc_flag_dissolved_oxygen_uncorrected_mg_per_l",
+    "qc_flag_salinity_psu",
+    "qc_flag_sensor_depth_measured_m",
+    "qc_flag_temperature_degree_c"
+  )
+
+  # save the origal data frame to join the qc_flag columns ti
+  if ("variable" %in% colnames(dat)) {
+    dat_og <- dat %>% qc_pivot_wider()
+
+  } else  dat_og <- dat
+
+  # pivot dat
   if (!("variable" %in% colnames(dat))) {
     dat <- qc_pivot_longer(dat, qc_tests = qc_tests)
   }
+
+  # use to join and sort the columns of the output
+  var_cols <- unique(dat$variable)
 
   # rename columns so depth_crosscheck_test doesn't affect max_flag
   if("depth_crosscheck_flag" %in% colnames(dat)) {
     dat <- rename(dat, depth_crosscheck = depth_crosscheck_flag)
   }
 
-  #vars <- unique(dat$variable)
-
+  # find the maximum flag for each variable
   dat <- dat %>%
     mutate(
       qc_col = pmap(select(dat, contains("flag")), max, na.rm = TRUE),
@@ -56,34 +103,33 @@ qc_assign_max_flag <- function(dat, qc_tests = NULL) {
 
   colnames(dat) <- str_remove_all(colnames(dat), pattern = "value_")
 
-  col_order <- c(
-    "county",
-    "waterbody",
-    "station",
-    "lease",
-    "latitude" ,
-    "longitude" ,
-    "deployment_range"   ,
-    "string_configuration",
-    "sensor_type"     ,
-    "sensor_serial_number"  ,
-    "timestamp_utc"  ,
-    "sensor_depth_at_low_tide_m",
-    "depth_crosscheck_flag"  ,
-    "dissolved_oxygen_percent_saturation"   ,
-    "dissolved_oxygen_uncorrected_mg_per_l",
-    "salinity_psu",
-    "sensor_depth_measured_m",
-    "temperature_degree_c",
-    "qc_flag_dissolved_oxygen_percent_saturation"   ,
-    "qc_flag_dissolved_oxygen_uncorrected_mg_per_l",
-    "qc_flag_salinity_psu",
-    "qc_flag_sensor_depth_measured_m",
-    "qc_flag_temperature_degree_c"
-  )
 
-  dat %>%
-    select(any_of(col_order), everything())
+  if(isTRUE(return_all)) {
+
+    join_cols <- c(
+      depl_cols[which(depl_cols %in% colnames(dat_og))], var_cols)
+
+    dat <- dat_og %>%
+      left_join(dat, by = join_cols) %>%
+      select(
+        any_of(depl_cols),    # deployment columns
+        all_of(var_cols),     # variable values
+        any_of(qc_test_cols), # qc flags
+        any_of(qc_max_cols),  # max qc flags
+        everything()          # anything left
+      )
+  } else {
+
+    dat <- dat %>%
+      select(
+        any_of(depl_cols),    # deployment columns
+        all_of(var_cols),     # variable values
+        any_of(qc_max_cols),  # max qc flags
+        everything()          # anything left
+      )
+  }
+
+  dat
 }
 
 # Assign each observation the maximum flag from applied QC tests.
--- tests/testthat/test-qc_pivot.R
+++ tests/testthat/test-qc_pivot.R
@@ -6,3 +6,9 @@ test_that("qc_pivot_longer() returns correct number of flag columns", {
 test_that("qc_pivot_wider() returns dat_wide", {
   expect_equal(dat_wide, dat_wide2)
 })
+
+test_that("qc_assign_max_flags() has correct number of columns", {
+
+  expect_equal(ncol(dat_wide) +  n_var, ncol(dat_wide_max))
+
+})
